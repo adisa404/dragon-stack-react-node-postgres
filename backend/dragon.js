37e80bc5ae2d421a918f756ad294bc2b40225aa8
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2020-01-25 16:05:01 +0100

    backend - create dragon class. Instantiate dragon object in index and log to console.

---
+++ backend/dragon.js
@@ -0,0 +1,8 @@
+class Dragon {
+  constructor(birthDate, nickname) {
+    this.birthDate = birthDate;
+    this.nickname = nickname;
+  }
+}
+
+module.exports = Dragon;
---
+++ backend/index.js
@@ -0,0 +1,5 @@
+const Dragon = require('./dragon');
+
+console.log('entry point');
+const dragon = new Dragon(new Date(), 'Disa');
+console.log(dragon);
