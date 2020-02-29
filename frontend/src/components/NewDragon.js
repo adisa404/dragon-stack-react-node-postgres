693b4c46440b69e6ae96e0ccc7e9bd1a3b66162d
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2020-02-29 09:46:41 +0100

    frontend - NewDragon component - display dragon values.

--- frontend/src/components/NewDragon.js
+++ frontend/src/components/NewDragon.js
@@ -1,23 +1,50 @@
 import React, { Component } from 'react';
 
+const DEFAULT_DRAGON = {
+  dragonId: '',
+  generationId: '',
+  birthDate: '',
+  traits: [],
+  nickname: 'nickname',
+};
 class NewDragon extends Component {
-  state = {};
+  state = { dragon: DEFAULT_DRAGON };
 
   fetchNewDragon = () => {
     fetch('http://localhost:3003/dragon/new')
       .then(response => response.json())
       .then(json => {
         console.log('json', json);
+        console.log('json.dragon', json.dragon);
+        this.setState({ dragon: json.dragon });
       })
       .catch(error => console.log(error));
   };
 
   componentDidMount() {
     this.fetchNewDragon();
+    console.log('traits', this.state.dragon.traits);
   }
 
   render() {
-    return <div>test</div>;
+    const {
+      dragonId,
+      nickname,
+      birthDate,
+      generationId,
+      traits,
+    } = this.state.dragon;
+
+    console.log('traits in render', traits);
+    return (
+      <div>
+        <p>{dragonId}</p>
+        <p>{nickname}</p>
+        <p>{birthDate}</p>
+        {this.state.dragon.traits.map(trait => trait.traitValue).join(', ')}
+        <p>{generationId}</p>
+      </div>
+    );
   }
 }
 
